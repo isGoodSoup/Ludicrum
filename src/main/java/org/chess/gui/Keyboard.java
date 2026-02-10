f97c17bf63cc719500fb30f9a5a7bf5035130b54
diego <sargent.pointed453@passinbox.com>
2026-02-10 18:52:01 +0100

    update: added keys tue 10-02 18:51

--- src/main/java/org/chess/gui/Keyboard.java
+++ src/main/java/org/chess/gui/Keyboard.java
@@ -4,7 +4,13 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Keyboard implements KeyListener {
+    private boolean wPressedEvent = false;
+    private boolean aPressedEvent = false;
+    private boolean sPressedEvent = false;
+    private boolean dPressedEvent = false;
     private boolean bPressedEvent = false;
+    private boolean zPressedEvent = false;
+    private boolean spacePressedEvent = false;
 
     @Override
     public void keyTyped(KeyEvent e) {}
@@ -12,13 +18,85 @@ public class Keyboard implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
-        if(key == KeyEvent.VK_B) {
+        if(key == KeyEvent.VK_W) {
+            wPressedEvent = true;
+        }
+
+        if(key == KeyEvent.VK_A) {
+            aPressedEvent = true;
+        }
+
+        if(key == KeyEvent.VK_S) {
+            sPressedEvent = true;
+        }
+
+        if(key == KeyEvent.VK_D) {
+            dPressedEvent = true;
+        }
+
+        if(key == KeyEvent.VK_SPACE) {
             bPressedEvent = true;
+        }
+
+        if(key == KeyEvent.VK_B) {
+            spacePressedEvent = true;
+        }
+
+        if(key == KeyEvent.VK_Z) {
+            zPressedEvent = true;
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {}
+
+    public boolean wasWPressed() {
+        if(wPressedEvent) {
+            wPressedEvent = false;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean wasAPressed() {
+        if(aPressedEvent) {
+            aPressedEvent = false;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean wasSPressed() {
+        if(sPressedEvent) {
+            sPressedEvent = false;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean wasDPressed() {
+        if(dPressedEvent) {
+            dPressedEvent = false;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean wasSpacePressed() {
+        if(spacePressedEvent) {
+            spacePressedEvent = false;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean wasZPressed() {
+        if(zPressedEvent) {
+            zPressedEvent = false;
+            return true;
+        }
+        return false;
+    }
 
     public boolean wasBPressed() {
         if(bPressedEvent) {
