1290a44d66c0bd97ce952a3a215712026d8e19b7
diego <sargent.pointed453@passinbox.com>
2026-02-18 23:23:02 +0100

    update: reworked achievement sprites wed 18-02 23:22

--- src/main/java/org/vertex/engine/enums/Achievements.java
+++ src/main/java/org/vertex/engine/enums/Achievements.java
@@ -1,8 +1,8 @@
 package org.vertex.engine.enums;
 
 public enum Achievements {
-    FIRST_CAPTURE           (1001L, "a01_first_capture",          "First Capture",          "Capture your first chess piece"),
-    SECRET_TOGGLE           (1002L, "a02_toggles",                "One Rule To Rule 'Em All","Find the (secret) toggle"),
+    FIRST_CAPTURE           (1001L, "a01_first_steps",            "First Steps",            "Capture your first chess piece"),
+    SECRET_TOGGLE           (1002L, "a02_toggles",                "The One To Rule 'Em All","Find the (secret) toggle"),
     CHECKMATE               (1003L, "a03_checkmate",              "Checkmate!",             "Win a game by checkmate"),
     CASTLING_MASTER         (1004L, "a04_castling_master",        "Oh My King",             "Castle at least 10 times"),
     KING_PROMOTER           (1005L, "a05_king_promoter",          "King Promoter",          "Promote the same pawn 4 times"),
@@ -13,18 +13,18 @@ public enum Achievements {
     HARD_GAME               (1010L, "a10_that_was_easy",          "That Was Easy!",         "Win a hard chess game"),
     UNTOUCHABLE             (1011L, "a11_cant_touch_this",        "Can't Touch This",       "Win a game without getting checked"),
     MASTER_OF_NONE          (1012L, "a12_master_of_none",         "Master of None",         "Complete all Chess achievements"),
-    ROUND_CAPTURE           (2001L, "a13_first_capture",          "First Capture",          "Capture your first checkers piece"),
+    ROUND_CAPTURE           (2001L, "a13_first_capture",          "Wait A Second",          "Capture your first checkers piece"),
     QUICK_START             (2002L, "a14_quick_start",            "Quick Start",            "Win a checkers game in under 8 moves"),
-    STRATEGIST              (2003L, "a15_strategist",             "Plastique Strategist",   "Win a checkers game without losing a single piece"),
-    DOUBLE_JUMP             (2004L, "a16_double_jump",            "Double Jump",            "Perform a double jump in one turn"),
-    TRIPLE_THREAT           (2005L, "a17_triple_threat",          "Triple Threat",          "Perform a triple jump in one turn"),
+    STRATEGIST              (2003L, "a15_strategist",             "Strategist",             "Win a checkers game without losing a single piece"),
+    DOUBLE_JUMP             (2004L, "a16_graysons",               "The Grayson's",          "Perform a double jump in one turn"),
+    TRIPLE_THREAT           (2005L, "a17_triple_threat",          "Three's Company",        "Perform a triple jump in one turn"),
     KING_ME                 (2006L, "a18_king_me",                "King Me",                "Promote your first piece to a King"),
-    KINGDOM_BUILDER         (2007L, "a19_kingdom_builder",        "Kingdom Come",           "Promote 3 pieces to Kings in a single game"),
-    COMEBACK                (2008L, "a20_comeback",               "Comeback",               "Win a game after being down by 3 or more pieces"),
-    DRAW_MASTER             (2009L, "a21_draw_master",            "Draw Master",            "End a game in a draw"),
-    CLEAN_SWEEP             (2010L, "a22_clean_sweep",            "Clean Sweep",            "Win a game by capturing all opponent pieces"),
-    STALEMATE_SURVIVOR      (2011L, "a23_stalemate_survivor",     "Last Of Us",             "Avoid losing when only 1 piece remains"),
-    CHAMPION                (2012L, "a24_checkers_champion",      "Checkers Champion",      "Complete all Checkers achievements"),
+    KINGDOM_BUILDER         (2007L, "a19_kingdom_builder",        "Kingdom Come",           "Promote 4 pieces to Kings in a single game"),
+    COMEBACK                (2008L, "a20_comeback",               "Comeback",               "Win a game after being down by 4 or more pieces"),
+    DRAW_MASTER             (2009L, "a21_even_stevens",           "Even Stevens",           "End a game in a draw"),
+    CLEAN_SWEEP             (2010L, "a22_lord_of_checkers",       "The Lord Of Checkers",   "Win a game by capturing all opponent pieces"),
+    STALEMATE_SURVIVOR      (2011L, "a23_stalemate_survivor",     "The Last Of Us",         "Avoid losing when only 1 piece remains"),
+    CHAMPION                (2012L, "a24_kingmaker",              "Kingmaker",              "Complete all Checkers achievements"),
     GRANDMASTER             (1024L, "axx_grandmaster",            "Grandmaster",            "Complete all achievements");
 
     private final long id;
