452f25e1824c5646bccd41f724c329cc1c4f49f9
diego <sargent.pointed453@passinbox.com>
2026-02-21 14:46:13 +0100

    fix: renamed TITLE static sat 21-02 14:46

--- src/main/java/org/lud/engine/gui/GameFrame.java
+++ src/main/java/org/lud/engine/gui/GameFrame.java
@@ -9,7 +9,7 @@ import java.io.Serial;
 public class GameFrame extends JFrame {
 	@Serial
     private static final long serialVersionUID = -3130387824420425271L;
-	private final static String TITLE = "VertexEngine";
+	private final static String TITLE = "Ludicrum";
 	private final BoardPanel panel;
 	private final GraphicsDevice gd;
 	private Rectangle windowedBounds;
