f8e3215e8c0e750e9d02744c9994a4f0f97473d9
diego <sargent.pointed453@passinbox.com>
2026-02-24 17:31:42 +0100

    fix: auto mode not enabled in hard mode tue 24-02 17:31

--- src/main/java/org/lud/engine/animations/ToastAnimation.java
+++ src/main/java/org/lud/engine/animations/ToastAnimation.java
@@ -18,7 +18,7 @@ public class ToastAnimation implements Animation {
     private static final double SLIDE_TIME = 0.3;
     private static final double STAY_TIME = 2.0;
     private static final int SLIDE_DISTANCE = 400;
-    private static final int WIDTH = 800;
+    private static final int WIDTH = 1000;
     private static final int HEIGHT = 150;
     private static final int ARC = 25;
     private int baseY;
