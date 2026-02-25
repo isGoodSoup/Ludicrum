53531f398e36c6a10a4940164d796b3470299f1d
diego <sargent.pointed453@passinbox.com>
2026-02-25 23:16:02 +0100

    update: wed 25-02 23:15

--- src/main/java/org/lud/engine/rulesets/CheckersRuleset.java
+++ src/main/java/org/lud/engine/rulesets/CheckersRuleset.java
@@ -18,8 +18,8 @@ public class CheckersRuleset implements Ruleset {
     private final BoardService boardService;
 
     private static final int[][] OFFSETS = {
-            {1, 1}, {1, -1}, {-1, 1}, {-1, -1},   // single moves
-            {2, 2}, {2, -2}, {-2, 2}, {-2, -2}    // jumps
+            {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+            {2, 2}, {2, -2}, {-2, 2}, {-2, -2}
     };
 
     public CheckersRuleset(PieceService pieceService, BoardService boardService) {
@@ -29,11 +29,11 @@ public class CheckersRuleset implements Ruleset {
 
     @Override
     public boolean isLegalMove(Piece p, int targetCol, int targetRow) {
-        if (!PieceService.isWithinBoard(targetCol, targetRow)) {
+        if(!PieceService.isWithinBoard(targetCol, targetRow)) {
             return false;
         }
 
-        if (PieceService.getPieceAt(targetCol, targetRow,
+        if(PieceService.getPieceAt(targetCol, targetRow,
                 pieceService.getPieces()) != null) {
             return false;
         }
@@ -45,11 +45,11 @@ public class CheckersRuleset implements Ruleset {
     public int evaluateMove(Move move) {
         Piece p = move.piece();
         int score = 1;
-        if (Math.abs(move.targetRow() - p.getRow()) == 2) {
+        if(Math.abs(move.targetRow() - p.getRow()) == 2) {
             score += 3;
         }
 
-        if (move.targetRow() == (p.getColor() == Turn.LIGHT ? 0 : 7)
+        if(move.targetRow() == (p.getColor() == Turn.LIGHT ? 0 : 7)
                 && p instanceof Checker c && !c.isKing()) {
             score += 5;
         }
@@ -60,12 +60,12 @@ public class CheckersRuleset implements Ruleset {
     public List<MoveScore> getAllLegalMoves(Turn color) {
         List<MoveScore> allMoves = new ArrayList<>();
         List<MoveScore> captureMoves = new ArrayList<>();
-        for (Piece p : pieceService.getPieces()) {
-            if (p.getColor() != color) continue;
-            for (int[] offset : OFFSETS) {
+        for(Piece p : pieceService.getPieces()) {
+            if(p.getColor() != color) continue;
+            for(int[] offset : OFFSETS) {
                 int newRow = p.getRow() + offset[0];
                 int newCol = p.getCol() + offset[1];
-                if (isLegalMove(p, newCol, newRow)) {
+                if(isLegalMove(p, newCol, newRow)) {
                     MoveScore ms = new MoveScore(new Move
                             (p, p.getCol(), p.getRow(), newCol, newRow, p.getColor(),
                                     p.getOtherPiece(), p.isPromoted(), p.getColor(), p.hasMoved(),
@@ -74,7 +74,7 @@ public class CheckersRuleset implements Ruleset {
                             p.getCol(), p.getRow(), newCol, newRow, p.getColor(),
                             p.getOtherPiece(), p.isPromoted(), p.getColor(), p.hasMoved(),
                                     p.getPreCol(), p.getPreRow(), p.isTwoStepsAhead())));
-                    if (Math.abs(offset[0]) == 2) captureMoves.add(ms);
+                    if(Math.abs(offset[0]) == 2) captureMoves.add(ms);
                     else allMoves.add(ms);
                 }
             }
@@ -87,11 +87,11 @@ public class CheckersRuleset implements Ruleset {
         int dRow = move.targetRow() - p.getRow();
         int dCol = move.targetCol() - p.getCol();
 
-        if (Math.abs(dRow) == 2 && Math.abs(dCol) == 2) {
+        if(Math.abs(dRow) == 2 && Math.abs(dCol) == 2) {
             int capturedRow = p.getRow() + dRow / 2;
             int capturedCol = p.getCol() + dCol / 2;
             Piece captured = PieceService.getPieceAt(capturedCol, capturedRow, pieceService.getPieces());
-            if (captured != null) pieceService.removePiece(captured);
+            if(captured != null) pieceService.removePiece(captured);
         }
 
         p.setRow(move.targetRow());
@@ -99,8 +99,8 @@ public class CheckersRuleset implements Ruleset {
         p.setX(move.targetCol() * Board.getSquare());
         p.setY(move.targetRow() * Board.getSquare());
 
-        if (p instanceof Checker c) {
-            if ((c.getColor() == Turn.LIGHT && c.getRow() == 0) || (c.getColor() == Turn.DARK && c.getRow() == 7)) {
+        if(p instanceof Checker c) {
+            if((c.getColor() == Turn.LIGHT && c.getRow() == 0) || (c.getColor() == Turn.DARK && c.getRow() == 7)) {
                 c.promoteToKing();
             }
         }
